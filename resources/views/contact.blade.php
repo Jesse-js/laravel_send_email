7e9e584232cb0f6a968d39c26f30e120471b8a1b
Jesse-js <jesse-jjs@hotmail.com>
2024-06-12 12:50:41 -0300

    feat: adds contact form

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
+    <title>Contact Us</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Contact Us</h1>
+        <p>This is the contact page.</p>
+        <form action="{{ route('contact.store') }}" method="post">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input name="name" type="text" class="form-control" id="name" placeholder="name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input name="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Message</label>
+                <textarea name="message" class="form-control" id="message" rows="3"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
